ae04046df291bbd4cb0d1c871846092239e7a84a
hh <792212010@qq.com>
2023-02-06 15:16:24 -0800

    fix bugs/change resume/add homepage button

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
         <div className="navigation-bar">
             <div className="mx-auto w-11/12 max-w-7xl pt-6 flex items-center justify-between">
                 <div className="sduration-300 hover:-translate-y-1 hover:cursor-pointer sm:inline-block">
-                    <span className="navbar-icon">Scarlett H.</span>
+                    <span className="navbar-icon"><a href="/">Scarlett H.</a></span>
                 </div>
                 <NavItems />
                 <Dropdown />
